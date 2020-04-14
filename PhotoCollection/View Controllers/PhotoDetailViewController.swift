1a7abbed0da385ec754df24b0b07e9111dbc98cf
Stephanie Ballard <user.name>
2020-04-13 22:08:20 -0700

    button, textfield, image view programmatic constraints written and save photo button created

--- PhotoCollection/View Controllers/PhotoDetailViewController.swift
+++ PhotoCollection/View Controllers/PhotoDetailViewController.swift
@@ -22,6 +22,7 @@ class PhotoDetailViewController: UIViewController, UIImagePickerControllerDelega
         
         setTheme()
         updateViews()
+        setUpSubviews()
     }
     
     // MARK: - UIImagePickerControllerDelegate
@@ -39,27 +40,46 @@ class PhotoDetailViewController: UIViewController, UIImagePickerControllerDelega
     
     private func setUpSubviews() {
         // Add an UIImageView to the view controller's view. You can leave its image empty in this function.
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        imageView.contentMode = UIView.ContentMode.scaleAspectFit
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .blue
         self.view.addSubview(imageView)
+        
+        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 400).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
         
         // Add a UIButton to the view controller's view. Set its title to "Add Image"
         // Add a target to it that calls the addImage function that already exists in this view controller.
         
-        let addImageButton = UIButton(type: .custom)
-        addImageButton.setTitle("Add Image", for: .normal)
+        let addImageButton = UIButton(type: .system)
+        addImageButton.setTitle("Add Photo", for: .normal)
         addImageButton.addTarget(self, action: #selector(addImage), for: .touchUpInside)
+        addImageButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(addImageButton)
+        
+        addImageButton.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 40).isActive = true
+        addImageButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        addImageButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
         
         // Add a UITextField to the view controller's view.
         // Set its placeholder to something like "Give this photo a title:". You can leave its text empty.
-        let titleTextField: UITextField = UITextField(frame: CGRect(x: 0, y: 0, width: 350, height: 30))
+        let titleTextField: UITextField = UITextField(frame: CGRect(x: 0, y: 0, width: 300, height: 30))
         titleTextField.placeholder = "Give this photo a title:"
         titleTextField.text = ""
         titleTextField.borderStyle = .line
         titleTextField.backgroundColor = UIColor.white
         titleTextField.textColor = UIColor.black
+        titleTextField.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(titleTextField)
         
+        titleTextField.topAnchor.constraint(equalTo: addImageButton.bottomAnchor, constant: 8).isActive = true
+        titleTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        titleTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        
+        let savePhotoButton = UIBarButtonItem(title: "Save Photo", style: .done, target: .none, action: #selector(savePhoto))
+        navigationItem.rightBarButtonItem = savePhotoButton
     }
     
     @objc private func addImage() {
@@ -85,7 +105,7 @@ class PhotoDetailViewController: UIViewController, UIImagePickerControllerDelega
         }
     }
     
-    private func savePhoto() {
+    @objc private func savePhoto() {
         
         guard let image = imageView.image,
             let imageData = image.pngData(),
